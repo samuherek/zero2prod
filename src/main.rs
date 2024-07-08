9d921086cc0acb8de223c338989c9d3ca4ff5562
Sam Uherek <samuherekbiz@gmail.com>
2024-07-08 18:39:04 +0200

    setup custom database for testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -6,7 +7,10 @@ use zero2prod::startup::run;
 
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Faield to read the configuration.");
+    let conn_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connec to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, conn_pool)?.await
 }
